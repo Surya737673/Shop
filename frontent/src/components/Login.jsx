855e864cffcdf77aec12c6b00c19f1c0c3367441
Surya Pratap Yadav <99668540+Surya737673@users.noreply.github.com>
2023-04-15 00:41:02 +0530

    first commit

---
+++ frontent/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='min-h-screen bg-grey-50 flex flex-col justify-center py-12 sm:px-6'>
+            <div className=''>
+
+            </div>
+    </div>
+  )
+}
+
+export default Login
