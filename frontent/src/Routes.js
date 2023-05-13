b92b7d86f5fbd5c754062d95fea086416e0d88d2
Surya Pratap Yadav <99668540+Surya737673@users.noreply.github.com>
2023-05-13 09:09:02 +0530

    homepage done

--- frontent/src/Routes.js
+++ frontent/src/Routes.js
@@ -1,5 +1,20 @@
-import LoginPage from "./pages/LoginPage.jsx"
+import LoginPage from "../src/pages/LoginPage";
+import SignupPage from "../src/pages/SignupPage";
+import ActivationPage from "../src/pages/ActivationPage";
+import HomePage from "../src/pages/HomePage";
+import ProductsPage from "../src/pages/ProductPage";
+import BestSellingPage from "../src/pages/BestSellingPage";
+import EventsPage from "../src/pages/EventsPage";
+import FAQPage from "../src/pages/FAQPage";
 
 export {
-    LoginPage
+    LoginPage,
+    SignupPage,
+    ActivationPage,
+    HomePage,
+    ProductsPage,
+    BestSellingPage,
+    EventsPage,
+    FAQPage
+
 }
