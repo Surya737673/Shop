b7ad8539f573c0f2260397428e5a9daee704bc73
Surya Pratap Yadav <99668540+Surya737673@users.noreply.github.com>
2023-11-24 12:04:26 +0530

    cors origin

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(logger('dev'));  //
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://shop-7ebcnpu84-surya737673.vercel.app/",
   credentials: true,
 }));
 app.use("/", express.static("uploads"));
